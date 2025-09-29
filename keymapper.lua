24d58706ddb66a829c759f78c786d51ab2fe9b3d
yeaminsarder <yeaminsardersr@gmail.com>
2025-09-29 22:18:39 +0600

    licence

--- keymapper.lua
+++ keymapper.lua
@@ -1,11 +1,32 @@
 local wezterm = require "wezterm"
 local inspect = require "inspect".inspect
 local m = {}
+m._LICENSE = [[
+  MIT LICENSE
 
 -- keys are something like LEADER|CTRL-h>LEADER|CTRL-p
+  Copyright (c) 2025 Md. Yeamin Sarder (yeaminsardersr@gmail.com)
 
 -- function m.mapraw(config, keys_table, action)
+  Permission is hereby granted, free of charge, to any person obtaining a
+  copy of this software and associated documentation files (the
+  "Software"), to deal in the Software without restriction, including
+  without limitation the rights to use, copy, modify, merge, publish,
+  distribute, sublicense, and/or sell copies of the Software, and to
+  permit persons to whom the Software is furnished to do so, subject to
+  the following conditions:
 
+  The above copyright notice and this permission notice shall be included
+  in all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+]]
 function m.map_simple(config, keys, action, apply_to)
    local apply_to = apply_to or config.keys
    local seq = {}
